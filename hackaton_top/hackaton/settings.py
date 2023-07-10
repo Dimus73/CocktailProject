1edde7e2301e3145cdddc156dc9985d5c682f99e
Dimus73 <dmitry.prigozhin@gmail.com>
2023-07-10 16:16:48 +0300

    23-07-10-04

--- hackaton_top/hackaton/settings.py
+++ hackaton_top/hackaton/settings.py
@@ -21,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a=xl6g@%r4k6-49znil8o6^@!uojq*ue324(8hjttva38cxi8w'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', default='django-insecure-a=xl6g@%r4k6-49znil8o6^@!uojq*ue324(8hjttva38cxi8w')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
